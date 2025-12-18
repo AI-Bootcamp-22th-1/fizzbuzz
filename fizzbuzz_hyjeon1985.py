73c2869c0bd820c255a3bf2feba08712bff2832e
J.Hoya <hyjeon1985@gmail.com>
2025-12-18 16:52:10 +0900

    refactor(fizzbuzz): Update fizzbuz function. (Print buzz)

--- fizzbuzz_hyjeon1985.py
+++ fizzbuzz_hyjeon1985.py
@@ -2,6 +2,8 @@ def fizzbuzz(start: int=1, end: int=31):
     for i in range(start, end + 1):
         if i % 3 == 0:
             print("fizz")
+        if i % 5 == 0:
+            print("buzz")
         else:
             print(i)
 
